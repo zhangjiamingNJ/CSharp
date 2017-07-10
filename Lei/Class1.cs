96b1397093419e91a8cdb1712b818fc662f8ffad
张嘉明 <Zhangjiamingitisme@outlook.com>
2017-07-10 10:40:21 +0800

    Add files via upload

---
+++ Lei/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2
+{
+    partial class point
+    {
+        public point(int ax,int ay,int bx,int by)
+        {
+            Ax = ax;
+            Bx = bx;
+            Ay = ay;
+            By = by;
+        }
+        public point()
+        {
+
+        }
+        public double cal()
+        {
+            return Math.Sqrt(Math.Pow(this.Ax - this.Bx, 2) + Math.Pow(this.Ay - this.By, 2));
+        }
+        public void set(int ax, int ay, int bx, int by)
+        {
+            Ax = ax;
+            Bx = bx;
+            Ay = ay;
+            By = by;
+        }
+    }
+}
---
+++ Lei/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2
+{
+    partial class point
+    {
+        private int Ax, Bx, Ay, By;
+    }
+}
